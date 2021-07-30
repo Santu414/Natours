687dc1c86a39591926402c63d098c2ca66d103b4
Santosh Reddy <snagankera@gmail.com>
2021-07-30 07:32:10 +0530

    Worked on Environment Variables

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(express.static(`${__dirname}/public`));
+console.log(process.env);
+if (process.env.NODE_EN === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
