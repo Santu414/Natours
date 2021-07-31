99d7000cbb4ba9b023e87cfd646875209369993d
Santosh Reddy <snagankera@gmail.com>
2021-07-31 05:54:39 +0530

    Worked on Sorting and Field Limiting

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -3,17 +3,36 @@ const Tour = require("../models/tourModels");
 
 exports.getAllTours = async (req, res) => {
   try {
-    const queryobj = { ...req.query };
-    const excludeFields = ["page", "sort", "limit", "fields"];
-    excludeFields.forEach((el) => delete queryobj[el]);
+    //Build Qury
+    //1A) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
     // 1B) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    let query = Tour.find(JSON.parse(queryStr));
 
-    const query = Tour.find(JSON.parse(queryStr));
+    //2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      console.log(sortBy);
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    //3) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
     const tours = await Tour.find(query);
-
     res.status(200).json({
       results: tours.length,
       status: "Success",
